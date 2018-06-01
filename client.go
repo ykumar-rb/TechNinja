3fb049ee204f46762d4306a917f0f5eedbab85dc
ykumar-rb <yogesh.kumar@riverbed.com>
2018-06-01 21:45:28 +0530

    client ui

--- client.go
+++ client.go
@@ -6,17 +6,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/icza/gowut/gwu"
-	"github.com/go-redis/redis"
 	"strconv"
+
+	"github.com/go-redis/redis"
+	"github.com/icza/gowut/gwu"
 )
 
-
 const (
-	Install = 1
+	Install  = 1
 	Rollback = 2
-	Update  =  3
-	NoAction = 4
+	Update   = 3
+	NoAction = 5
 )
 
 const (
@@ -24,131 +24,89 @@ const (
 	Failure = 2
 )
 
-type SoftwareDB struct{
-	Name string
-	Version string
+type SoftwareDB struct {
+	Name         string
+	Version      string
 	AvailVersion string
-	Action int
-	Status int
+	Action       int
+	Status       int
 }
 
-func SetDataListInDB(client *redis.Client, SDBList []*SoftwareDB){
+func SetDataListInDB(client *redis.Client, SDBList []*SoftwareDB) {
 
 	for _, SDB := range SDBList {
 
-		SetDataInDB(client,SDB)
+		SetDataInDB(client, SDB)
 	}
 }
 
-func SetDataInDB(client *redis.Client, SDB *SoftwareDB){
-	client.HSet(SDB.Name,"Name",SDB.Name)
-	client.HSet(SDB.Name,"Version",SDB.Version)
-	client.HSet(SDB.Name,"AvailVersion", SDB.AvailVersion)
-	client.HSet(SDB.Name,"Action", SDB.Action)
-	client.HSet(SDB.Name,"Status", SDB.Status)
+func SetDataInDB(client *redis.Client, SDB *SoftwareDB) {
+	client.HSet(SDB.Name, "Name", SDB.Name)
+	client.HSet(SDB.Name, "Version", SDB.Version)
+	client.HSet(SDB.Name, "AvailVersion", SDB.AvailVersion)
+	client.HSet(SDB.Name, "Action", SDB.Action)
+	client.HSet(SDB.Name, "Status", SDB.Status)
 }
 
-
-func PrepareKubernetesSetupDummyData() []*SoftwareDB{
+func PrepareKubernetesSetupDummyData() []*SoftwareDB {
 	SDBList := []*SoftwareDB{}
 
 	SDB1 := &SoftwareDB{}
 	SDB1.Status = Success
 	SDB1.Action = Update
-	SDB1.Name ="kubernetes"
-	SDB1.Version ="1.9.3-00"
-	SDB1.AvailVersion ="1.10.3"
+	SDB1.Name = "kubernetes"
+	SDB1.Version = "1.9.3-00"
+	SDB1.AvailVersion = "1.10.3"
 
 	SDB2 := &SoftwareDB{}
 	SDB2.Status = Success
-	SDB2.Action = Install
-	SDB2.Name ="docker-ce"
-	SDB2.Version ="17.03.2~ce-0~ubuntu-xenial"
-	SDB2.AvailVersion ="17.03.2~ce-0~ubuntu-xenial"
+	SDB2.Action = NoAction
+	SDB2.Name = "docker-ce"
+	SDB2.Version = "17.03.2~ce-0~ubuntu-xenial"
+	SDB2.AvailVersion = "17.03.2~ce-0~ubuntu-xenial"
 
 	SDBList = append(SDBList, SDB1)
 	SDBList = append(SDBList, SDB2)
 	return SDBList
 }
 
-func PrepareKubernetesKeyList()( keyList[]string){
+func PrepareKubernetesKeyList() (keyList []string) {
 
-	keyList = []string{"kubernetes","docker-ce"}
+	keyList = []string{"kubernetes", "docker-ce"}
 	return
 }
-
 
 func DatabaseOperation(client *redis.Client, win gwu.Window) {
 	SDPDataLIst := PrepareKubernetesSetupDummyData()
 	SetDataListInDB(client, SDPDataLIst)
 
-	/*
-	Building software database
-	SDB := SoftwareDB{}
-	SDB.Status = Success
-	SDB.Action = Update
-	SDB.Name ="kubernetes"
-	SDB.Version ="1.9.3-00"
-	SDB.AvailVersion ="1.10.3"
-
-	fmt.Println("DataBase Client Function")
-	err := client.Set(SDB.Name, SDB.Name, 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := client.Get(SDB.Name).Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Software Name: ", val)
-	fmt.Println("   ########################################################  ")
-
-	SetDataInDB(client,&SDB)
-
-    name := client.HGet(SDB.Name,"Name")
-    version := client.HGet(SDB.Name,"Version")
-	availVersion := client.HGet(SDB.Name,"AvailVersion")
-	action := client.HGet(SDB.Name,"Action")
-	status := client.HGet(SDB.Name,"Status")
-	fmt.Printf("*** Software Details # Name: %s, Version: %s AvailVersion: %s Action:%v, Status: %v",
-		name.Val(),version.Val(), availVersion.Val(), action.Val(),status.Val())
-
-	data := client.HGetAll(SDB.Name)
-	fmt.Printf("All Value: %+v",data)
-*/
-
-
-    // Display software details at Ninja Client UI
+	// Display software details at Ninja Client UI
 	keyList := PrepareKubernetesKeyList()
-	DisplayAtNinjaClientUI(client,win,keyList)
+	DisplayAtNinjaClientUI(client, win, keyList)
 }
 
-func GetDataFromDataBase(key string, client *redis.Client)(out SoftwareDB){
+func GetDataFromDataBase(key string, client *redis.Client) (out SoftwareDB) {
 	fmt.Printf("PNP Client DATABASE ")
 	outSDB := SoftwareDB{}
-	outSDB.Name = client.HGet(key,"Name").Val()
-	outSDB.Version = client.HGet(key,"Version").Val()
-	outSDB.AvailVersion = client.HGet(key,"AvailVersion").Val()
-	outSDB.Action, _ = strconv.Atoi(client.HGet(key,"Action").Val())
-	outSDB.Status ,_= strconv.Atoi(client.HGet(key,"Status").Val())
-
+	outSDB.Name = client.HGet(key, "Name").Val()
+	outSDB.Version = client.HGet(key, "Version").Val()
+	outSDB.AvailVersion = client.HGet(key, "AvailVersion").Val()
+	outSDB.Action, _ = strconv.Atoi(client.HGet(key, "Action").Val())
+	outSDB.Status, _ = strconv.Atoi(client.HGet(key, "Status").Val())
 
 	fmt.Printf("*** Software Details # Name: %s, Version: %s AvailVersion: %s Action:%v, Status: %v",
-		outSDB.Name,outSDB.Version, outSDB.AvailVersion, outSDB.Action,outSDB.Status)
+		outSDB.Name, outSDB.Version, outSDB.AvailVersion, outSDB.Action, outSDB.Status)
 
 	data := client.HGetAll(key)
-	fmt.Printf("All Value: %+v",data)
+	fmt.Printf("All Value: %+v", data)
 	return outSDB
 }
 
-func DisplayAtNinjaClientUI(client *redis.Client, win gwu.Window, keyList []string){
-
-
+func DisplayAtNinjaClientUI(client *redis.Client, win gwu.Window, keyList []string) {
+	// Fetching data from Database for all keys
 	var sdb [2]SoftwareDB
-	sdb[0] = GetDataFromDataBase(keyList[0],client)
-	sdb[1] = GetDataFromDataBase(keyList[1],client)
-
+	sdb[0] = GetDataFromDataBase(keyList[0], client)
+	sdb[1] = GetDataFromDataBase(keyList[1], client)
 
 	p := gwu.NewPanel()
 	p.SetHAlign(gwu.HACenter)
@@ -157,19 +115,15 @@ func DisplayAtNinjaClientUI(client *redis.Client, win gwu.Window, keyList []stri
 	t := gwu.NewTable()
 	t.Style().SetBorder2(10, gwu.BrdStyleSolid, gwu.ClrNavy)
 	t.SetAlign(gwu.HARight, gwu.VATop)
-	t.Style().SetSize("1000","500")
-	t.EnsureSize(5, 5)
+	t.Style().SetSize("1000", "500")
+	t.EnsureSize(2, 2)
 	t.RowFmt(0).Style().SetBackground(gwu.ClrNavy)
 
 	t.RowFmt(0).SetAlign(gwu.HADefault, gwu.VAMiddle)
 	t.RowFmt(1).SetAlign(gwu.HADefault, gwu.VAMiddle)
-	t.RowFmt(2).SetAlign(gwu.HADefault, gwu.VAMiddle)
-	t.RowFmt(3).SetAlign(gwu.HADefault, gwu.VAMiddle)
-	t.RowFmt(4).SetAlign(gwu.HADefault, gwu.VAMiddle)
-
 
 	img := gwu.NewImage(fmt.Sprintf("Installed Software"), "http://www2.multilizer.com/wp-content/uploads/2014/07/tool.jpg")
-	img.Style().SetSize("70","50")
+	img.Style().SetSize("70", "50")
 	t.Add(img, 0, 0)
 
 	lb1 := gwu.NewLabel(fmt.Sprintf("Current Version"))
@@ -189,38 +143,36 @@ func DisplayAtNinjaClientUI(client *redis.Client, win gwu.Window, keyList []stri
 	lb4.Style().SetColor("white")
 	lb4.Style().SetWidth("20")
 
-
 	t.Add(lb1, 0, 1)
 	t.Add(lb2, 0, 2)
 	t.Add(lb3, 0, 3)
 	t.Add(lb4, 0, 4)
 
 	btnsPanel := gwu.NewNaturalPanel()
-
 	for row := 1; row < 3; row++ {
-		t.Add(gwu.NewLabel(fmt.Sprintf("%s", sdb[row -1].Name)), row, 0)
-		t.Add(gwu.NewLabel(fmt.Sprintf("%s", sdb[row -1].Version)), row, 1)
-		t.Add(gwu.NewLabel(fmt.Sprintf("%s", sdb[row -1].AvailVersion)), row, 2)
+		t.Add(gwu.NewLabel(fmt.Sprintf("%s", sdb[row-1].Name)), row, 0)
+		t.Add(gwu.NewLabel(fmt.Sprintf("%s", sdb[row-1].Version)), row, 1)
+		t.Add(gwu.NewLabel(fmt.Sprintf("%s", sdb[row-1].AvailVersion)), row, 2)
 
 		var statusStr string
-		if sdb[row -1].Status == Success{
+		if sdb[row-1].Status == Success {
 			statusStr = "Operation Success"
-		}else{
+		} else {
 			statusStr = "Operation Failure"
 		}
 		t.Add(gwu.NewLabel(fmt.Sprintf("%s", statusStr)), row, 3)
 
 		var actionStr string
-		if sdb[row -1].Action == Install {
+		if sdb[row-1].Action == Install {
 			actionStr = "INSTALL"
-		}else if sdb[row -1].Action == Rollback{
+		} else if sdb[row-1].Action == Rollback {
 			actionStr = "ROLLBACK"
-		}else if sdb[row -1].Action == Update{
+		} else if sdb[row-1].Action == Update {
 			actionStr = "UPDATE"
-		}else if sdb[row -1].Action == NoAction{
-			actionStr= "NOACTION"
-		}else{
-			actionStr=""
+		} else if sdb[row-1].Action == NoAction {
+			actionStr = "NOACTION"
+		} else {
+			actionStr = ""
 		}
 
 		butn1 := gwu.NewButton(fmt.Sprintf("%s", actionStr))
@@ -228,30 +180,20 @@ func DisplayAtNinjaClientUI(client *redis.Client, win gwu.Window, keyList []stri
 		butn1.Style().SetBackground("green")
 
 		butn1.AddEHandlerFunc(func(e gwu.Event) {
-
-			if butn1.Text()  == "UPDATE"{
+			if butn1.Text() == "UPDATE" {
 				fmt.Printf("UPDATE button pressed!")
-			}else if butn1.Text() == "INSTALL"{
+			} else if butn1.Text() == "INSTALL" {
 				fmt.Printf("INSTALL button pressed!")
-			}else if butn1.Text() == "ROLLBACK"{
+			} else if butn1.Text() == "ROLLBACK" {
 				fmt.Printf("ROLLBACK button pressed!")
-			}else if butn1.Text() == "NOACTION"{
+			} else if butn1.Text() == "NOACTION" {
 				fmt.Printf("NOACTION button pressed!")
-			}else{
+			} else {
 				fmt.Printf("UNKNOWN button pressed!")
 			}
-
-			//newbtn := gwu.NewButton(fmt.Sprintf("Created Environment #%d", btnsPanel.CompsCount()))
-			//btnsPanel.Insert(newbtn, 0)
 		}, gwu.ETypeClick)
 
-		t.Add(butn1,row,4)
-	}
-
-	for row := 2; row < 5; row++ {
-		for col := 0; col < 5; col++ {
-			t.Add(gwu.NewLabel(fmt.Sprintf("Button %d%d", row, col)), row, col)
-		}
+		t.Add(butn1, row, 4)
 	}
 
 	p.Add(t)
@@ -281,11 +223,11 @@ func main() {
 	win.Style().SetFullWidth()
 	win.SetHAlign(gwu.HACenter)
 	win.SetCellPadding(2)
-	
+
 	p := gwu.NewPanel()
 	p.SetHAlign(gwu.HACenter)
 	p.SetCellPadding(2)
-        l2 := gwu.NewLabel("PNP Software Catalog")
+	l2 := gwu.NewLabel("PNP Software Catalog")
 	l2.Style().SetFontWeight(gwu.FontWeightBold).SetFontSize("300%")
 	l2.Style().SetColor("green")
 	l2.Style().SetBackground("while")
@@ -301,7 +243,7 @@ func main() {
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
-	DatabaseOperation(client,win)
+	DatabaseOperation(client, win)
 
 	// Adding all windows to server
 	server := gwu.NewServer("techninja.com", "localhost:8081")
